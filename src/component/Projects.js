8dc6225c4518d157346f02e6803ff9fca2df0843
rkddusen <k99endus@naver.com>
2023-03-24 14:38:29 +0900

    create Projects component

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -1,10 +1,44 @@
 import React from "react";
 import styled from 'styled-components';
+import Project from './Project';
 
 function Projects() {
   return (
-    <div></div>
+    <StyledProjects>
+      <ProjectsTitle>
+        <Title>Projects</Title>
+      </ProjectsTitle>
+      <ProjectsContent>
+        <ProjectsList>
+          <Project style={{top: "0", left: "0"}}></Project>
+          <Project style={{bottom: "0", right: "0"}}></Project>
+        </ProjectsList>
+        <ProjectsList>
+          <Project style={{top: "0", left: "0"}}></Project>
+          <Project style={{bottom: "0", right: "0"}}></Project>
+        </ProjectsList>
+      </ProjectsContent>
+    </StyledProjects>
   );
 }
 
+const StyledProjects = styled.div`
+  width: 100%;
+  background-color: #f1f1f1;
+`;
+const ProjectsTitle = styled.div`
+  padding: 100px 0;
+`;
+const Title = styled.p`
+  text-align: center;
+  font-size: 40px;
+`;
+const ProjectsContent = styled.div`
+  width: 100%;
+  padding-bottom: 100px;
+`;
+const ProjectsList = styled.div`
+  height: 1000px;
+  position: relative;
+`;
 export default Projects;
